e7a510b14d304eb3e0eb3641a01eb4fe1f005a5a
fatiEnGit <157553539+fatiEnGit@users.noreply.github.com>
2024-01-31 00:13:51 -0300

    Cambio de if por switch

--- Desafío 1/app.js
+++ Desafío 1/app.js
@@ -1,108 +1,100 @@
-let contenedor = ["ai", "enter", "imes", "ober", "ufat"];
-let caracteresEspeciales = /[^\w\sñ]/g;
-let enviar = document.querySelector(".enviar");
+const contenedor = ["ai", "enter", "imes", "ober", "ufat"];
+const caracteresEspeciales = /[^\w\sñ]/g;
+
+const enviar = document.querySelector(".enviar");
 enviar.addEventListener("click", function () {
-  let input = document.querySelector(".input");
-  let inputLower = input.value.toLowerCase();
-  let resultado = document.querySelector(".resultado");
-  let areaOutput = document.querySelector(".area-output");
+  const input = document.querySelector(".input");
+  const inputLower = input.value.toLowerCase();
+  const resultado = document.querySelector(".resultado");
+  const areaOutput = document.querySelector(".area-output");
   let newInput = "";
+
   if (inputLower === input.value && !caracteresEspeciales.test(input.value)) {
     for (let i = 0; i < input.value.length; i++) {
-      if (input.value[i] === "a") {
-        newInput += contenedor[0];
-      } else if (input.value[i] === "e") {
-        newInput += contenedor[1];
-      } else if (input.value[i] === "i") {
-        newInput += contenedor[2];
-      } else if (input.value[i] === "o") {
-        newInput += contenedor[3];
-      } else if (input.value[i] === "u") {
-        newInput += contenedor[4];
-      } else {
-        newInput += input.value[i];
+      switch (input.value[i]) {
+        case "a":
+          newInput += contenedor[0];
+          break;
+        case "e":
+          newInput += contenedor[1];
+          break;
+        case "i":
+          newInput += contenedor[2];
+          break;
+        case "o":
+          newInput += contenedor[3];
+          break;
+        case "u":
+          newInput += contenedor[4];
+          break;
+        default:
+          newInput += input.value[i];
+          break;
       }
     }
 
-    resultado.innerHTML = newInput;
+    resultado.textContent = newInput;
     areaOutput.style.backgroundImage = "none";
   } else {
     alert("Solo se aceptan letras minúsculas y sin caracteres especiales");
   }
 });
 
-let reset = document.querySelector(".reset");
+const reset = document.querySelector(".reset");
 reset.addEventListener("click", function () {
-  let input = document.querySelector(".input");
-  let resultado = document.querySelector(".resultado");
-  let areaOutput = document.querySelector(".area-output");
+  const input = document.querySelector(".input");
+  const resultado = document.querySelector(".resultado");
+  const areaOutput = document.querySelector(".area-output");
   input.value = "";
-  resultado.innerHTML = "";
+  resultado.textContent = "";
   areaOutput.style.backgroundImage = 'url("fondo.png")';
 });
 
 function updateClipboard() {
-  let resultado = document.querySelector(".resultado");
-  navigator.clipboard.writeText(resultado.innerHTML).then(() => {
+  const resultado = document.querySelector(".resultado");
+  navigator.clipboard.writeText(resultado.textContent).then(() => {
     alert("Copiado al portapapeles");
   });
 }
 
-let copiar = document.querySelector(".copiar");
+const copiar = document.querySelector(".copiar");
 copiar.addEventListener("click", updateClipboard);
 
-let desencriptar = document.querySelector(".desencriptar");
+const desencriptar = document.querySelector(".desencriptar");
 desencriptar.addEventListener("click", function () {
-  let input = document.querySelector(".input");
-  let inputLower = input.value.toLowerCase();
-  let resultado = document.querySelector(".resultado");
-  let areaOutput = document.querySelector(".area-output");
+  const input = document.querySelector(".input");
+  const inputLower = input.value.toLowerCase();
+  const resultado = document.querySelector(".resultado");
+  const areaOutput = document.querySelector(".area-output");
   let newInput = "";
   let i = 0;
+
   if (inputLower === input.value && !caracteresEspeciales.test(input.value)) {
     while (i < input.value.length) {
-      if (input.value[i] === "a" && input.value[i + 1] === "i") {
-        newInput += "a";
-        i += 2;
-      } else if (
-        input.value[i] === "e" &&
-        input.value[i + 1] === "n" &&
-        input.value[i + 2] === "t" &&
-        input.value[i + 3] === "e" &&
-        input.value[i + 4] === "r"
-      ) {
-        newInput += "e";
-        i += 5;
-      } else if (
-        input.value[i] === "i" &&
-        input.value[i + 1] === "m" &&
-        input.value[i + 2] === "e" &&
-        input.value[i + 3] === "s"
-      ) {
-        newInput += "i";
-        i += 4;
-      } else if (
-        input.value[i] === "o" &&
-        input.value[i + 1] === "b" &&
-        input.value[i + 2] === "e" &&
-        input.value[i + 3] === "r"
-      ) {
-        newInput += "o";
-        i += 4;
-      } else if (
-        input.value[i] === "u" &&
-        input.value[i + 1] === "f" &&
-        input.value[i + 2] === "a" &&
-        input.value[i + 3] === "t"
-      ) {
-        newInput += "u";
-        i += 4;
-      } else {
-        newInput += input.value[i];
-        i++;
+      switch (input.value.substr(i, 5)) {
+        case "enter":
+          newInput += "e";
+          i += 5;
+          break;
+        case "imes":
+          newInput += "i";
+          i += 4;
+          break;
+        case "ober":
+          newInput += "o";
+          i += 4;
+          break;
+        case "ufat":
+          newInput += "u";
+          i += 4;
+          break;
+        default:
+          newInput += input.value[i];
+          i++;
+          break;
       }
     }
-    resultado.innerHTML = newInput;
+    resultado.textContent = newInput;
     areaOutput.style.backgroundImage = "none";
   } else {
     alert("Solo se aceptan letras minúsculas y sin caracteres especiales");
