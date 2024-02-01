adc042e944511f16a2ec6fb7537eb96d92cf9e4d
fatiEnGit <157553539+fatiEnGit@users.noreply.github.com>
2024-02-01 15:01:16 -0300

    Si no hay input no cambia el fondo

--- Desafío 1/app.js
+++ Desafío 1/app.js
@@ -1,63 +1,55 @@
-const contenedor = ["ai", "enter", "imes", "ober", "ufat"];
-const caracteresEspeciales = /[^\w\sñ]/g;
-
-const enviar = document.querySelector(".enviar");
+let contenedor = ["ai", "enter", "imes", "ober", "ufat"];
+let caracteresEspeciales = /[^\w\sñ]/g;
+let enviar = document.querySelector(".enviar");
 enviar.addEventListener("click", function () {
-  const input = document.querySelector(".input");
-  const inputLower = input.value.toLowerCase();
-  const resultado = document.querySelector(".resultado");
-  const areaOutput = document.querySelector(".area-output");
+  let input = document.querySelector(".input");
+  let inputLower = input.value.toLowerCase();
+  let resultado = document.querySelector(".resultado");
+  let areaOutput = document.querySelector(".area-output");
   let newInput = "";
-
   if (inputLower === input.value && !caracteresEspeciales.test(input.value)) {
     for (let i = 0; i < input.value.length; i++) {
-      switch (input.value[i]) {
-        case "a":
-          newInput += contenedor[0];
-          break;
-        case "e":
-          newInput += contenedor[1];
-          break;
-        case "i":
-          newInput += contenedor[2];
-          break;
-        case "o":
-          newInput += contenedor[3];
-          break;
-        case "u":
-          newInput += contenedor[4];
-          break;
-        default:
-          newInput += input.value[i];
-          break;
+      if (input.value[i] === "a") {
+        newInput += contenedor[0];
+      } else if (input.value[i] === "e") {
+        newInput += contenedor[1];
+      } else if (input.value[i] === "i") {
+        newInput += contenedor[2];
+      } else if (input.value[i] === "o") {
+        newInput += contenedor[3];
+      } else if (input.value[i] === "u") {
+        newInput += contenedor[4];
+      } else {
+        newInput += input.value[i];
       }
     }
-
-    resultado.textContent = newInput;
-    areaOutput.style.backgroundImage = "none";
+    if (input.value !== "") {
+      resultado.innerHTML = newInput;
+      areaOutput.style.backgroundImage = "none";
+    }
   } else {
     alert("Solo se aceptan letras minúsculas y sin caracteres especiales");
   }
 });
 
-const reset = document.querySelector(".reset");
+let reset = document.querySelector(".reset");
 reset.addEventListener("click", function () {
-  const input = document.querySelector(".input");
-  const resultado = document.querySelector(".resultado");
-  const areaOutput = document.querySelector(".area-output");
+  let input = document.querySelector(".input");
+  let resultado = document.querySelector(".resultado");
+  let areaOutput = document.querySelector(".area-output");
   input.value = "";
-  resultado.textContent = "";
+  resultado.innerHTML = "";
   areaOutput.style.backgroundImage = 'url("fondo.png")';
 });
 
 function updateClipboard() {
-  const resultado = document.querySelector(".resultado");
-  navigator.clipboard.writeText(resultado.textContent).then(() => {
+  let resultado = document.querySelector(".resultado");
+  navigator.clipboard.writeText(resultado.innerHTML).then(() => {
     alert("Copiado al portapapeles");
   });
 }
 
-const copiar = document.querySelector(".copiar");
+let copiar = document.querySelector(".copiar");
 copiar.addEventListener("click", updateClipboard);
 
 let desencriptar = document.querySelector(".desencriptar");
